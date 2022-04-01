0f36419ab66b4a53b70ac75af5704137da16342a
marcusungarette <marcusungarette@gmail.com>
2022-03-31 22:02:38 -0300

    Added MovieCell with ContainerView constraints

---
+++ MoviesAPP/Screens/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  MoviesAPP
+//
+//  Created by Marcus on 31/03/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
